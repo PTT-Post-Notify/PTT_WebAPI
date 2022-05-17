e006678b815da21f46889d1d98564e4996fba3b4
MakotoAtsu <chris03152008@gmail.com>
2022-05-18 07:44:40 +0800

    Added Swagger UI

--- ptt_webapi/urls.py
+++ ptt_webapi/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+swagger = get_schema_view(
+    openapi.Info(
+        title="PTT WebAPI",
+        description="Provide RESTFul API to access ptt.cc",
+        default_version='v1',
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(email="contact@snippets.local"),
+        # license=openapi.License(name="BSD License"),
+    ),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', swagger.with_ui()),
 ]
