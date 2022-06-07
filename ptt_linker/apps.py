0e8473b729b2cae89bc10d06efa7bfe90325db6c
MakotoAtsu <chris03152008@gmail.com>
2022-06-07 18:09:47 +0800

    Init memory cache

--- ptt_linker/apps.py
+++ ptt_linker/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PttLinkerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ptt_linker'
+    from django.core.cache import cache
+    cache.set('CacheInit', True, None)
--- ptt_linker/views/boardClass.py
+++ ptt_linker/views/boardClass.py
@@ -1,10 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from ptt_linker.service import ClassService
-from ptt_linker.serializer import BoardNodeSerializer
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from django.core.cache import cache
+from ptt_linker.service import ClassService
+from ptt_linker.serializer import BoardNodeSerializer
 
 
 @api_view(['GET'])
@@ -24,6 +25,7 @@ def get_particular_class(req: Request, cls: int):
 
     _recu: str = req.query_params.get('recursive') or 'False'
     recu = True if _recu.lower() == "true" else False
+
     data = ClassService.fetch_class_boards(cls, recu)
     result = BoardNodeSerializer(data, many=True).data
     return Response(result)
